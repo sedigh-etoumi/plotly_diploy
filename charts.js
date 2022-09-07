57d451c46fffabf59d244717cc55ba89ebb438eb
Sedigh Etoumi <sed_elm@hotmail.com>
2022-09-07 01:54:29 -0700

    message

--- charts.js
+++ charts.js
@@ -1,36 +1,35 @@
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
-
+​
   // Use the list of sample names to populate the select options
   d3.json("samples.json").then((data) => {
     var sampleNames = data.names;
-    console.log(data);
-
+​
     sampleNames.forEach((sample) => {
       selector
         .append("option")
         .text(sample)
         .property("value", sample);
     });
-
+​
     // Use the first sample from the list to build the initial plots
     var firstSample = sampleNames[0];
     buildCharts(firstSample);
     buildMetadata(firstSample);
   });
 }
-
+​
 // Initialize the dashboard
 init();
-
+​
 function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildMetadata(newSample);
   buildCharts(newSample);
-  
+​
 }
-
+​
 // Demographics Panel 
 function buildMetadata(sample) {
   d3.json("samples.json").then((data) => {
@@ -38,110 +37,80 @@ function buildMetadata(sample) {
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
+​
     // Use d3 to select the panel with id of `#sample-metadata`
     var PANEL = d3.select("#sample-metadata");
-
+​
     // Use `.html("") to clear any existing metadata
     PANEL.html("");
-
+​
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
     Object.entries(result).forEach(([key, value]) => {
       PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
     });
-
+​
   });
 }
-
-// 1. Create the buildCharts function.
-var demographicInfo = [];
-var sortedOtu = []
+​
+// Create the buildChart function.
 function buildCharts(sample) {
-  // 2. Use d3.json to load and retrieve the samples.json file 
+  // Use d3.json to load the samples.json file 
   d3.json("samples.json").then((data) => {
-    // 3. Create a variable that holds the samples array. 
-    var metadata = data.metadata;
-    var sample = data.samples;
-  
-    // 4. Create a variable that filters the samples for the object with the desired sample number.
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    var resultList = sample.filter(sampleObj => sampleObj.id == sample);
-    
-    //  5. Create a variable that holds the first sample in the array.
+    console.log(data);
+​
+    // Create a variable that holds the samples array. 
+    var samples = data.samples;
+    // console.log(samples);
+​
+    // Create a variable that filters the samples for the object with the desired sample number.
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+​
+    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var metadataArray = data.metadata.filter(sampleObj => sampleObj.id == sample);
+​
+    // Create a variable that holds the first sample in the array.
     var result = resultArray[0];
-    var sampleResult = resultList[0];
-
-  
-    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
-    var otu_ids = sampleResult.otu_ids;
-    var otu_labels = sampleResult.otu_labels;
-    var sampleValues =  sampleResult.sample_values;
-
-    // 7. Create the yticks for the bar chart.
-    // Hint: Get the the top 10 otu_ids and map them in descending order  
-    //  so the otu_ids with the most bacteria are last. 
-
-    
-    toptenArraysorted = otu_ids.sort((a,b) =>
-    a.otu_ids - b.otu_ids.reverse());
-    
-    var topTen = otu_ids.slice(0,10 );
-    var toptenArray = topTen.map(element => element.otu_ids);
-    console.log(toptenArray);
-
-    toptenArraysamplevalues_sorted = sampleValues.sort((a,b) =>
-    a.sampleValues - b.sampleValues.reverse());
-     
-    var toptenarraySample = sampleValues.slice(0,10);
-    var toptenArray = topTen.map(element => element.otu_ids);
-    console.log(toptenArray);
-   
-    
-    var yticks = toptenArray
-
-    // 8. Create the trace for the bar chart. 
-    var barData = {
-      x: toptenarraySample,
-      y: toptenArray,
-      type: "bar"
-
-    }
-      
-          
-    // 9. Create the layout for the bar chart. 
+​
+    // 2. Create a variable that holds the first sample in the metadata array.
+    var metadata = metadataArray[0];
+​
+    // Create variables that hold the otu_ids, otu_labels, and sample_values.
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
+​
+    // 3. Create a variable that holds the washing frequency.
+    var frequency = parseFloat(metadata.wfreq);
+​
+​
+    // Create the yticks for the bar chart.
+    // Hint: Get the the top 10 otu_ids and map them in descending order 
+    // so the otu_ids with the most bacteria are last. 
+    var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+    // Create the trace for the bar chart. 
+    var barData = [
+      {
+        y: yticks,
+        x: sample_values.slice(0, 10).reverse(),
+        text: otu_labels.slice(0, 10).reverse(),
+        type: "bar",
+        orientation: "h",
+      }
+    ];
+    // Create the layout for the bar chart. 
     var barLayout = {
       title: "Top 10 Bacteria Cultures Found",
-      xaxis: {title: "Sample Values"},
-      yaxis: {title: "OTU's"}
-     
+      margin: { t: 30, l: 150 }
     };
-    // 10. Use Plotly to plot the data with the layout.   
-
-    Plotly.newPlot("bar", barData, barLayout); 
-
+    // Use Plotly to plot the data with the layout. 
+    Plotly.newPlot("bar", barData, barLayout);
+​
 });
  
 }
 
-/*     var trace = {
- x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
- y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
- type: "bar"
-};
-var data = [trace];
-var layout = {
- title: "'Bar' Chart",
- xaxis: {title: "Drinks"},
- yaxis: {title: "% of Drinks Ordered"}             
-};
-Plotly.newPlot("plotArea", data, layout);                                                                           
 
-
-      var sortedCities = cityGrowths.sort((a,b) =>
-    a.Increase_from_2016 - b.Increase_from_2016).reverse(); 
-
-*/
 
 
